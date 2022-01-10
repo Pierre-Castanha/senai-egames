721a09f5536b2f94f06630af536d7915e95e31e8
Pierre Castanha <adv.pierrecastanha@gmail.com>
2022-01-10 17:34:44 -0300

    atualização 10.01.2021

--- js/script.js
+++ js/script.js
@@ -1,18 +1,24 @@
-$(document).ready
-    let slideAtual=1;
-    let listaSlides=["banner-imagem1", "banner-imagem2", "banner-imagem3", "banner-imagem4", "banner-imagem5"];
+$(document).ready(function () {
+
+    let slideAtual = 1;
+    let listaSlides = ["banner-imagem1", "banner-imagem2", "banner-imagem3", "banner-imagem4", "banner-imagem5"];
 
     setInterval(slide, 1500);
 
-    function slide(){
-        console.log("slideAtual:", slideAtual);
+    function slide() {
+        console.log("slide Atual:", slideAtual)
 
-        if (slideAtual>0) {
-            $ ("#carrossel".removeClass(listaSlides[slideAtual -1]))
-        } else if (slideAtual==0){
-            $ ("#carrossel".removeClass(listaSlides[(listaSlides.length)-1]))
+        if (slideAtual > 0) {
+            $("#carrossel").removeClass(listaSlides[slideAtual - 1])
+        } else if (slideAtual == 0) {
+            $("#carrossel").removeClass(listaSlides[(listaSlides.length) - 1])
         }
-        $ ("#carrossel").addClass(listaSlides[slideAtual])
+        $("#carrossel").addClass(listaSlides[slideAtual])
         slideAtual++;
+
+        if (slideAtual > 4) {
+            slideAtual = 0;
         }
+
+    }
 })
